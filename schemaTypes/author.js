394565db9eec00758acc1df1fe7308447f2ca716
developeralamgirbd <developer.alamgirbd@gmail.com>
2024-11-02 13:56:09 +0600

    [Done] autor slug add

--- schemaTypes/author.js
+++ schemaTypes/author.js
@@ -10,6 +10,20 @@ export default
         type: 'string'
       },
       {
+        title: 'Slug',
+        name: 'slug',
+        type: 'slug',
+        options: {
+          source: 'name',
+          maxLength: 200, // will be ignored if slugify is set
+          slugify: input => input
+            .toLowerCase()
+            .replace(/\s+/g, '-')
+            .slice(0, 200)
+        },
+        validation: Rule => Rule.required()
+      },
+      {
         name: 'avatar',
         title: 'Avatar',
         type: 'image'
